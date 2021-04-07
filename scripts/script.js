e300542cf3231ec1586c8161cd18ba0a36af5952
Adam <am1macdonald@gmail.com>
2021-04-06 21:57:34 -0400

    logic pushes numbers to display

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,27 @@
-function calculate() {
+function numInput() {
     let numArr = [];
     let numberButton = document.querySelectorAll(".number");
     numberButton.forEach(button => {
-        button.addEventListener('click', event => {
-            if (button.name === '.') {
-                if (numArr.includes('.')){
+        button.addEventListener('click', (event) => {
+            if (numArr.length < 12) {
+                if (button.name === '.') {
+                    if (numArr.includes('.')){
+                        return;
+                    } else if (numArr.length === 0) {
+                        numArr.push('0', '.');
+                    } else {
+                        numArr.push('.');
+                    };
+                } else if (button.name === '0' && numArr.length === 0){
                     return;
-                } else if (numArr.length === 0) {
-                    numArr.push('0', '.');
-                } else {
-                    numArr.push('.');
+                } else if (typeof parseInt(button.name) === 'number') {
+                    numArr.push(button.name);
                 };
-            } else if(button.name === '0' && numArr.length === 0){
-                return;
-            } else if (typeof parseInt(button.name) === 'number') {
-                console.log('if');
-                numArr.push(button.name);
-            };          
-
             console.log(button.name, numArr);
+            document.getElementById('display').innerText = numArr.join('');
+            } else return;
         });
     });
 };
 
-calculate();
+numInput();

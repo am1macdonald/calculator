71498d0d0fb9411e978a8d1734968d0650c5a70a
Adam <am1macdonald@gmail.com>
2021-04-07 05:58:28 -0400

    fixed script to make numArr accessible to other functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,26 @@
-function numInput() {
-    let numArr = [];
-    let numberButton = document.querySelectorAll(".number");
-    numberButton.forEach(button => {
-        button.addEventListener('click', (event) => {
-            if (numArr.length < 12) {
-                if (button.name === '.') {
-                    if (numArr.includes('.')){
-                        return;
-                    } else if (numArr.length === 0) {
-                        numArr.push('0', '.');
-                    } else {
-                        numArr.push('.');
-                    };
-                } else if (button.name === '0' && numArr.length === 0){
+let numArr = [];
+let numberButton = document.querySelectorAll(".number");
+numberButton.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if (numArr.length < 12) {
+            if (button.name === '.') {
+                if (numArr.includes('.')){
                     return;
-                } else if (typeof parseInt(button.name) === 'number') {
-                    numArr.push(button.name);
+                } else if (numArr.length === 0) {
+                    numArr.push('0', '.');
+                } else {
+                    numArr.push('.');
                 };
-            console.log(button.name, numArr);
-            document.getElementById('display').innerText = numArr.join('');
-            } else return;
-        });
+            } else if (button.name === '0' && numArr.length === 0){
+                return;
+            } else if (typeof parseInt(button.name) === 'number') {
+                numArr.push(button.name);
+            };
+        console.log(button.name, numArr);
+        document.getElementById('display').innerText = numArr.join('');
+        } else return;
     });
-};
+});
+
 
 numInput();

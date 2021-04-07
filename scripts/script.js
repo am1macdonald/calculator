fbe1b77c6023a1d24f337d36900f6b3f5850c405
Adam <am1macdonald@gmail.com>
2021-04-06 21:19:17 -0400

    added logic for numbers

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,23 @@ function calculate() {
     let numberButton = document.querySelectorAll(".number");
     numberButton.forEach(button => {
         button.addEventListener('click', event => {
+            if (button.name === '.') {
+                if (numArr.includes('.')){
+                    return;
+                } else if (numArr.length === 0) {
+                    numArr.push('0', '.');
+                } else {
+                    numArr.push('.');
+                };
+            } else if(button.name === '0' && numArr.length === 0){
+                return;
+            } else if (typeof parseInt(button.name) === 'number') {
+                console.log('if');
+                numArr.push(button.name);
+            };          
 
             console.log(button.name, numArr);
-        })    
+        });
     });
 };
 
